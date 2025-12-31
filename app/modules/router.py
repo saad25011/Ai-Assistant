389842186cf353978c77ddb01d8cb8d2efe854ef
Muhammad Saad khan <106921642+saad25011@users.noreply.github.com>
2025-12-31 21:22:54 +0500

    adding new tools in ai assistant

--- app/modules/router.py
+++ app/modules/router.py
@@ -3,4 +3,5 @@ from fastapi import APIRouter
 from app.modules.Ai_voice_assistent.api.assistant import v1 as ai_voice_assistant_router    
 router = APIRouter()
 # Register routers
+
 router.include_router(ai_voice_assistant_router)
