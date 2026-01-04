3a21729eec1eb69361d4c4920f066531029e8d88
Muhammad Saad khan <106921642+saad25011@users.noreply.github.com>
2026-01-04 22:30:19 +0500

    adding router

--- app/modules/router.py
+++ app/modules/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 # Import all routers
-from app.modules.Ai_voice_assistent.api.assistant import v1 as ai_voice_assistant_router    
+from app.modules.Ai_voice_assistent.api.assistant import v1 as ai_voice_assistant_router   
+
+ # Register routers
 router = APIRouter()
-# Register routers
 
 router.include_router(ai_voice_assistant_router)
